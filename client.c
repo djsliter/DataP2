828771b4c2f2e1fa2b86b2e584978cf5009f7be2
brayma26 <77712700+brayma26@users.noreply.github.com>
2022-12-01 01:08:34 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,19 +1,41 @@
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include<stdarg.h>
+#include<pthread.h>
 
-#define MAXLINE 4096 /*max text line length*/
+//Mariah Bray, Dakota Sliter, and Selmir lelak
+
+#define MAXLINE 4196 /*max text line length*/
 #define SERV_PORT 3000 /*port*/
+
+int sockfd;
+char sendline[MAXLINE], recvline[MAXLINE];
+bool canrun = true;
+
+void *do_read(void *arg);
+void *do_write(void *arg);
 
 int main(int argc, char **argv) {
     
-    int sockfd;
+    pthread_t thread1;     //thread for reader
+    pthread_t thread2;     //thread for writer
+    int thr1;
+    int thr2;
     struct sockaddr_in servaddr;
-    char sendline[MAXLINE], recvline[MAXLINE];
+    
         
     //basic check of the arguments
     //additional checks can be inserted
@@ -41,20 +63,52 @@ int main(int argc, char **argv) {
         exit(3);
     }
         
-    while (fgets(sendline, MAXLINE, stdin) != NULL) {
-        
-        send(sockfd, sendline, strlen(sendline), 0);
-                
-        if (recv(sockfd, recvline, MAXLINE,0) == 0) {
-            //error: server terminated prematurely
-            perror("The server terminated prematurely"); 
-            exit(4);
-        }
-
-        printf("%s", "String received from the server: ");
-        fputs(recvline, stdout);
-
+    
+    if ((thr1 = pthread_create(&thread1, NULL, do_read, NULL)) != 0) {
+        fprintf(stderr, "Thread create error %d: %s\n", thr1, strerror(thr1));
+        exit(1);
+    }
+    pthread_detach(thread1);
+    if ((thr2 = pthread_create(&thread2, NULL, do_write, NULL)) != 0) {
+        fprintf(stderr, "Thread create error %d: %s\n", thr2, strerror(thr2));
+        exit(1);
     }
 
+    //wait for thread two to complete
+    pthread_join(thread2,NULL);
+
     exit(0);
+}
+
+//read takes data from server and prints
+void *do_read(void *arg){
+    while(canrun){
+        if (recv(sockfd, recvline, MAXLINE,0) == 0) {
+            if(canrun){
+                //error: server terminated prematurely
+                perror("The server terminated prematurely"); 
+                exit(4);
+            }
+        }
+
+        printf("%s", "From server: ");
+        fputs(recvline, stdout);
+        //clear string
+        memset(recvline, 0 , MAXLINE);
+    }
+    pthread_exit(NULL);
+}
+
+//write takes data from user and sends to sever
+void *do_write(void *arg){ 
+    while(canrun){ 
+        while (fgets(sendline, MAXLINE, stdin)!=NULL) {
+            if(strcmp(sendline,"quit\n")==0){
+                canrun = false;
+            }
+            send(sockfd, sendline, strlen(sendline), 0);
+            memset(sendline , 0 , MAXLINE);
+        }   
+    }
+    pthread_exit(NULL);
 }
